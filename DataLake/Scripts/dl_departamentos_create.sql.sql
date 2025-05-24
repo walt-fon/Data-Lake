06b935b25621a21ab32cd5d42bc9a1bb44fd8ebc
walt-fon <walter.fontechad@gmail.com>
2025-05-23 21:09:33 -0500

    Add files via upload

---
+++ DataLake/Scripts/dl_departamentos_create.sql.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Datalake.dbo.DL_Departamentos (
+	COD_DPTO tinyint NULL,
+	NOM_DPTO varchar(60) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	LATITUD float NULL,
+	LONGITUD float NULL,
+	[Geo Departamento] varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+);
