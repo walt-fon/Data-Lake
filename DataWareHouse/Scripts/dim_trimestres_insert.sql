c96a45f8eb221cb386ae308149a07e8c15d7f89a
walt-fon <walter.fontechad@gmail.com>
2025-05-27 16:52:24 -0500

    Add files via upload

---
+++ DataWareHouse/Scripts/dim_trimestres_insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO DataWareHouse.dbo.DIM_Trimestres 
+(NomTrimestre, NomCortoTrimestre)
+VALUES
+('Trimestre 1', 'TRIM 1'),
+('Trimestre 2', 'TRIM 2'),
+('Trimestre 3', 'TRIM 3'),
+('Trimestre 4', 'TRIM 4')
