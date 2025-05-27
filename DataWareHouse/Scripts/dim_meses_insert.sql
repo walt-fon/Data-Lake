4738a0b4d9421134301847865cfa57a39017902b
walt-fon <walter.fontechad@gmail.com>
2025-05-27 16:47:39 -0500

    Add files via upload

---
+++ DataWareHouse/Scripts/dim_meses_insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO DataWareHouse.dbo.DIM_Meses 
+(NomMes, NomCortoMes)
+VALUES
+('Enero', 'ENE'),
+('Febrero', 'FEB'),
+('Marzo', 'MAR'),
+('Abril', 'ABR'),
+('Mayo', 'MAY'),
+('Junio', 'JUN'),
+('Julio', 'JUL'),
+('Agosto', 'AGO'),
+('Septiembre', 'SEP'),
+('Octubre', 'OCT'),
+('Noviembre', 'NOV'),
+('Diciembre', 'DIC')
