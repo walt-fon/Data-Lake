11ab8e5db7181eaf3ca4ea7cfe97e4e4e14a320f
walt-fon <walter.fontechad@gmail.com>
2025-05-27 16:44:49 -0500

    Add files via upload

---
+++ DataWareHouse/Scripts/dim_dias_semana_insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO DataWareHouse.dbo.DIM_Dias_Semana
+(NomDiaSemana, NomCortoDiaSemana)
+VALUES
+('Lunes', 'LUN'),
+('Martes', 'MAR'),
+('Miércoles', 'MIE'),
+('Jueves', 'JUE'),
+('Viernes', 'VIE'),
+('Sábado', 'SAB'),
+('Domingo', 'DOM')
